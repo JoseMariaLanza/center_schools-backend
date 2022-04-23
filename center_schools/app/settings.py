121076c8e2344dcebe688cb53b364ef96d190373
Jose Maria Lanza <lanza.josemaria@gmail.com>
2022-04-23 17:36:10 -0300

    FEATURE | Custom User Model created.

--- center_schools/app/settings.py
+++ center_schools/app/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+AUTH_USER_MODEL = 'core.User'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
