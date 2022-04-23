f38478ca97afc5dcedb953a0dd6b2ac44d463f35
Jose Maria Lanza <lanza.josemaria@gmail.com>
2022-04-23 16:15:25 -0300

    UPDATE | Model test cases for user account.

--- center_schools/core/tests/test_models.py
+++ center_schools/core/tests/test_models.py
@@ -22,3 +22,17 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email, '123456')
 
         self.assertEqual(user.email, emali.lower())
+
+    def test_new_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, '123456')
+
+    def test_create_new_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'test@mail.com',
+            '123456'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
